18831a37b26ada019a52f0defb29d9cfde30238b
prouraist <prouraist@mail.ru>
2017-01-21 18:40:40 +0300

    Add files via upload

--- d.cpp
+++ d.cpp
@@ -22,16 +22,29 @@ string d(string s)
 		else if (!bracket)
 		{
 			if (s[i]=='+')
-				return "(" + d(s.substr(0,i)) + ")+(" + d(s.substr(i+1)) + ")";
+				return "((" + d(s.substr(0,i)) + ")+(" + d(s.substr(i+1)) + "))";
 			if (s[i]=='-')
-				return "(" + d(s.substr(0,i)) + ")-(" + d(s.substr(i+1)) + ")";
+				return "((" + d(s.substr(0,i)) + ")-(" + d(s.substr(i+1)) + "))";
+		}
+	}
+
+	bracket=0;
+
+	for(int i = s.size()-1; i>=0; i--)
+	{
+		if (s[i]=='(')
+			bracket++;
+		else if (s[i]==')')
+			bracket--;
+		else if (!bracket)
+		{
 			if (s[i]=='*' && (s[i - 1] != '*') && (s[i + 1] != '*')){
-				return "(" + d(s.substr(0, i)) + ")*(" + s.substr(i+1) + ")+(" +
-				   	   s.substr(0, i) + ")*(" + d(s.substr(i+1)) + ")";
+				return "((" + d(s.substr(0, i)) + ")*(" + s.substr(i+1) + ")+(" +
+				   	   s.substr(0, i) + ")*(" + d(s.substr(i+1)) + "))";
 			}
 			if (s[i]=='/')
-				return "((" + d(s.substr(0, i)) + ")*(" + s.substr(i + 1) + ")-(" +
-					   s.substr(0, i) + ")*(" + d(s.substr(i + 1)) + "))/((" + s.substr(i + 1) + ")**2)";
+				return "(((" + d(s.substr(0, i)) + ")*(" + s.substr(i + 1) + ")-(" +
+					   s.substr(0, i) + ")*(" + d(s.substr(i + 1)) + "))/((" + s.substr(i + 1) + ")**2))";
 		}
 	}
 
@@ -54,12 +67,11 @@ string d(string s)
             for(int u=0; u<x.size(); u++)
                 if (x[u]=='x') o2 = true;
 
-            if (o1 && o2)
-                return "(" + s + ")*(" + d(s.substr(i + 2) + "*ln(" + s.substr(0, i) + ")") + ")";
-            if (o1)
-                return "((" + s + ")*ln(" + x + ")*(" + d(st) + "))";
-            if (o2)
+            if (!o1)
                 return "(" + st + ")*((" + x + ")**(" + st + "-1))*(" + d(x) + ")";
+            else
+                return "(" + s + ")*(" + d(s.substr(i + 2) + "*ln(" + s.substr(0, i) + ")") + ")";
+
 		}
 	}
 
@@ -67,37 +79,37 @@ string d(string s)
 		return d(s.substr(1, s.size() - 2));
 
 	if ((int)s.find("ln")==0)
-		return "1/" + s.substr(2) + "*(" + d(s.substr(2)) + ")";
+		return "(1/" + s.substr(2) + "*(" + d(s.substr(2)) + "))";
 
 	if ((int)s.find("arctg")==0)
-		return "1/(1+" + s.substr(5) + "**2)*("+ d(s.substr(5)) + ")";
+		return "(1/(1+" + s.substr(5) + "**2)*("+ d(s.substr(5)) + "))";
 
 	if ((int)s.find("arcctg")==0)
-		return "(-1)/(1+" + s.substr(6) + "**2)*(" + d(s.substr(6)) + ")";
+		return "((-1)/(1+" + s.substr(6) + "**2)*(" + d(s.substr(6)) + "))";
 
 	if ((int)s.find("arcsin")==0)
-		return "1/((1-" + s.substr(6) + "**2)**0.5)*(" + d(s.substr(6)) + ")";
+		return "(1/((1-" + s.substr(6) + "**2)**0.5)*(" + d(s.substr(6)) + "))";
 
 	if ((int)s.find("arccos")==0)
-		return "(-1)/((1-" + s.substr(6) + "**2)**0.5)*(" + d(s.substr(6)) + ")";
+		return "((-1)/((1-" + s.substr(6) + "**2)**0.5)*(" + d(s.substr(6)) + "))";
 
 	if ((int)s.find("sin")==0)
-		return "cos" + s.substr(3) + "*(" + d(s.substr(3)) + ")";
+		return "(cos" + s.substr(3) + "*(" + d(s.substr(3)) + "))";
 
 	if ((int)s.find("cos")==0)
-		return "(-sin" + s.substr(3) + ")*(" + d(s.substr(3)) + ")";
+		return "((-sin" + s.substr(3) + ")*(" + d(s.substr(3)) + "))";
 
 	if ((int)s.find("ctg")==0)
-		return "(-1)/(sin" + s.substr(3) + "**2)*(" + d(s.substr(3)) + ")"
+		return "((-1)/(sin" + s.substr(3) + "**2)*(" + d(s.substr(3)) + "))"
 			;
 	if ((int)s.find("tg")==0)
-		return "1/(cos" + s.substr(2) + "**2)*(" + d(s.substr(2)) + ")";
+		return "(1/(cos" + s.substr(2) + "**2)*(" + d(s.substr(2)) + "))";
 
 
 	if (s == "x")
-		return "1";
+		return "(1)";
 
-	return "0";
+	return "(0)";
 }
 
 
